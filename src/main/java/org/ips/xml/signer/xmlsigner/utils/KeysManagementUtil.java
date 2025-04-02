29927e55c67f54fcf3c305242e53e11e5429507f
tttesfaye <teklutesfayeh@gmail.com>
2025-04-02 04:28:55 +0300

    fix Resolving XML external entity in user-controlled data

--- src/main/java/org/ips/xml/signer/xmlsigner/utils/KeysManagementUtil.java
+++ src/main/java/org/ips/xml/signer/xmlsigner/utils/KeysManagementUtil.java
@@ -2,5 +2,9 @@ package org.ips.xml.signer.xmlsigner.utils;
 
 public class KeysManagementUtil {
 
-    private String key
+    private String keyStoragePath;
+
+    private String storePassword;
+
+    private String keyPassword;
 }
